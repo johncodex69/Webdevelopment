f0f8ee1e5f63dd738312cca1d4e587dff2b4f805
unknown <johnngugi2005@gmail.com>
2023-03-10 10:54:06 +0300

    javascript

---
+++ javascript/index.js
@@ -0,0 +1,55 @@
+// /*Create variables*/
+// let name="Delvin"
+//     let number
+//         let age=13
+//         let num="150000000km"
+
+// console.log(name)
+// console.log(number)
+// console.log(age)
+// console.log(num)
+
+// let first_name="Rimuru"
+// let second_name="Tempest"
+// let full_name=first_name+second_name
+
+// console.log(full_name)
+
+// let status1=true
+// let status2=false
+
+// console.log(status1 && status2)
+
+// status1=true
+// console.log(!status1)
+
+// num1=20
+// num2=45
+// console.log(num1==num2)
+// console.log(num1!=num2)
+
+// let num=20
+// if(num<10) {//Code if condition is true
+//     console.log("Number is less than 10")
+// }else{//Code if condition is false
+//     console.log("Number is greter than 10")
+// }
+
+// num_1=15
+
+// if(num_1) {
+//     console.log("Greater than 20")
+// }else if(num_1==10) {
+//     console.log("less than 20")
+// }
+
+// let country="Germany"
+// let age=18
+
+// if(country=="Germany") {
+//     //code if true
+//     if(age>=18) {
+//         console.log("Accepted")
+//     }
+// }
+
